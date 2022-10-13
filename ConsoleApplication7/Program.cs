9e259eac5c9b9f52e3aacd093f8ffbed0eeb6e6f
Yunus Furkan Özer <bardakta.kahve@gmail.com>
2022-10-13 22:35:18 +0300

    math

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string bos = " ", mesaj;
+            Console.WriteLine("Ne yazdırmak istiyorsunuz? :");
+            mesaj = Console.ReadLine();
+            Console.WriteLine("Kaç defa yazdırmak istiyorsunuz? :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.WriteLine(mesaj);
+                Console.Write(bos);
+                bos = bos + " ";
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
